6285a622f9ec26fecc19262cb8d975efc6eab2a8
Fizi Yadav <fizi@Fizis-MacBook-Pro.local>
2015-11-08 19:36:05 -0500

    initial commit

--- CSVReader/main.cpp
+++ CSVReader/main.cpp
@@ -15,17 +15,18 @@
 #include <sys/stat.h>
 #include <assert.h>
 
+
 size_t getFilesize(const char* filename) {
     struct stat st;
     stat(filename, &st);
     return st.st_size;
 }
 
-int main(int argc, const char * argv[]) {
+void read(){
     // insert code here...
-    size_t filesize = getFilesize("Users/fizi/Documents/XCode/CSVReader/CSVReader/test.csv");
+    size_t filesize = getFilesize("test.csv");
     //Open file
-    int fd = open("Users/fizi/Documents/XCode/CSVReader/CSVReader/test.csv", O_RDONLY, 0);
+    int fd = open("test.csv", O_RDONLY, 0);
     assert(fd != -1);
     //Execute mmap
     void* mmappedData = mmap(NULL, filesize, PROT_READ, MAP_PRIVATE, fd, 0);
@@ -36,4 +37,29 @@ int main(int argc, const char * argv[]) {
     int rc = munmap(mmappedData, filesize);
     assert(rc == 0);
     close(fd);
+    
 }
+
+int write(){
+    
+    int filedesc = open("test.txt", O_WRONLY | O_APPEND);
+    if(filedesc < 0)
+        return 1;
+    
+    if(write(filedesc,"This will be output to testfile.txt\n", 36) != 36)
+    {
+        std::cout<<"There was an error writing to testfile.txt\n";    // strictly not an error, it is allowable for fewer characters than requested to be written.
+        return 1;
+    }
+    
+    return 0;
+    
+}
+
+int main(int argc, const char * argv[]) {
+    read();
+    return 0;
+    
+}
+
+
